ecc987df29a06e1a484e2ce11f58595cb08cf77b
Levin Gsell <levin.gsell@bluewin.ch>
2023-08-24 10:08:38 +0200

    Initial commit

---
+++ NotesApp/NotesApp/Library.fs
@@ -0,0 +1,5 @@
+﻿namespace NotesApp
+
+module Say =
+    let hello name =
+        sprintf "Hello %s" name
---
+++ NotesApp/NotesAppUI/Form1.cs
@@ -0,0 +1,10 @@
+namespace NotesAppUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
